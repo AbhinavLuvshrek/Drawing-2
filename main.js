75509ecaa71dd29368b16a3a63cd3e1031b47676
AbhinavLuvshrek <113375203+AbhinavLuvshrek@users.noreply.github.com>
2023-01-11 11:23:12 -0700

    fdas

--- main.js
+++ main.js
@@ -31,3 +31,12 @@ ctx.fillRect(150, 100, 150, 550);
 ctx.fillRect(170, 40, 110, 60);
 // Small Top
 ctx.fillRect(190, 20, 70, 20);
+
+//Loops
+
+window.onload = draw;
+
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(165, 130, 15, 490);
+}
